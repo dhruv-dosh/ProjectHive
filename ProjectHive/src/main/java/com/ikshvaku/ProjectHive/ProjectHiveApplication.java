a63d7e22085487c172ab83336bf95cf6b39a99aa
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2025-02-18 15:04:20 +0530

    Adding Backend Folder

---
+++ ProjectHive/src/main/java/com/ikshvaku/ProjectHive/ProjectHiveApplication.java
@@ -0,0 +1,13 @@
+package com.ikshvaku.ProjectHive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHiveApplication.class, args);
+	}
+
+}
