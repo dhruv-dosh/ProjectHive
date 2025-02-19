7ce5449f2d160af6d7aaa816e83a8ce1045025b9
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2025-02-19 23:30:06 +0530

    Adding Getter And setter Methods Manually.

--- ProjectHive/src/main/java/com/ikshvaku/ProjectHive/Response/AuthResponse.java
+++ ProjectHive/src/main/java/com/ikshvaku/ProjectHive/Response/AuthResponse.java
@@ -11,4 +11,19 @@ public class AuthResponse {
     private String jwt;
     private String message;
 
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
